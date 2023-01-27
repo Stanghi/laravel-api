0d0d3a4bde2fd411764dc7fd2e7556f5bc6f6c95
Stanghi <stangherlin.matti@gmail.com>
2023-01-27 17:52:08 +0100

    add function show for api

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -13,4 +13,17 @@ class ProjectController extends Controller
         $projects = Project::with(['type', 'technologies'])->orderBy('id', 'desc')->get();
         return response()->json(compact('projects'));
     }
+
+    public function show($slug)
+    {
+        $project = Project::where('slug', $slug)->with(['type', 'technologies'])->first();
+
+        if ($project->cover_image) {
+            $project->cover_image = url("storage/" . $project->cover_image);
+        } else {
+            $project->cover_image = url("storage/uploads/placeholder.png");
+        }
+
+        return response()->json($project);
+    }
 }
